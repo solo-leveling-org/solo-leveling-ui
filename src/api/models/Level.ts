675d613659b12325997bb0a3c698574056d1025d
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-02 21:14:55 +0300

    Refactored authentication and added user/player API endpoints.

---
+++ src/api/models/Level.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { Assessment } from './Assessment';
+export type Level = {
+    id?: string;
+    version?: number;
+    level?: number;
+    totalExperience?: number;
+    currentExperience?: number;
+    experienceToNextLevel?: number;
+    assessment?: Assessment;
+};
+
---
+++ src/api/models/TaskTopic.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export enum TaskTopic {
+    PHYSICAL_ACTIVITY = 'PHYSICAL_ACTIVITY',
+    MENTAL_HEALTH = 'MENTAL_HEALTH',
+    EDUCATION = 'EDUCATION',
+    CREATIVITY = 'CREATIVITY',
+    SOCIAL_SKILLS = 'SOCIAL_SKILLS',
+    HEALTHY_EATING = 'HEALTHY_EATING',
+    PRODUCTIVITY = 'PRODUCTIVITY',
+    EXPERIMENTS = 'EXPERIMENTS',
+    ECOLOGY = 'ECOLOGY',
+    TEAMWORK = 'TEAMWORK',
+}
